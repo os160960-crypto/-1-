94dfe442c0a8dc20aacc8ef11ad5782e7a96b2ba
오세진 <os160960@gmail.com>
2025-10-15 19:54:39 +0900

    자바스크립트 1일차

---
+++ 실습var/script.js
@@ -0,0 +1,44 @@
+/*
+	1. name1 변수를 생성
+	2. name1 변수에 "짱구"를 넣으세요.
+
+	3. age1 변수를 생성하고 5를 넣으세요.
+
+*/
+
+
+
+/*
+	theBtn1 변수를 생성하고 클래스 cha1을 theBtn1 변수에 넣으세요.
+
+*/
+
+
+/*
+	
+	theBtn1 변수를 클릭하면 h1이 name1 변수로 바뀌는 
+	함수를 만드세요.
+		
+		Hint.
+		
+		- h1을 선택하는 방법
+		document.querySelector("h1")
+		
+		- h1의 값을 선택하는 방법
+		document.querySelector("h1").innerHTML
+
+
+		함수란?
+		특별한 목적의 작업을 수행하도록 설계된 하나의 실행단위
+*/
+
+
+theBtn1.addEventListener("click", function(){
+	
+})
+
+
+
+
+
+
